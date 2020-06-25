88c5b5fc9def0d67e421c911d226d0866fa83208
Vojtěch Voleman <vojtavol69@gmail.com>
2020-06-25 20:02:32 +0200

    Procfile for Heroku

--- app/User.php
+++ app/User.php
@@ -5,13 +5,11 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use AjCastro\EagerLoadPivotRelations\EagerLoadPivotTrait;
 use Illuminate\Support\Facades\DB;
 
 class User extends Authenticatable
 {
     use Notifiable;
-    use EagerLoadPivotTrait;
 
     protected $hidden = ['password', 'remember_token',];
     protected $guarded = ['id_u'];
